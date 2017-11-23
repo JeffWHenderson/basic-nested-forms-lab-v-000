28b9701a4f5cdfef31982e0abca58b4a6df3237a
jeffrey henderson <jeffrey.henderson17@gmail.com>
2017-11-23 18:10:36 +0000

    automatically backed up by learn

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,7 +3,7 @@ class Recipe < ActiveRecord::Base
   accepts_nested_attributes_for :ingredients
 end
 
-#              //////// this is the hash that my form sends to as params ////////////////
+#              /////////// this is the hash that my form sends to as params ////////////////
 # {
 #   "utf8"=>"✓",
 #        "authenticity_token"=>"ctbg/E9TvCLG9G93s9v7Nm3dUFDKHigSWSEnL6h2Hikea9JbGJEBpmxxA9DINyd9PljQtSru3g8zxZjXHJG3NA==",
@@ -12,7 +12,7 @@ end
 #                                                   "0"=>  {"name"=>"chocolate", "quantity"=>"2 cups"},
 #                                                   "1"=> {"name"=>"vanilla", "quantity"=>"1 cup"}
 #                                                 }
-#             },
+#                   },
 #
 #  "commit"=>"Create Recipe"
 # }
