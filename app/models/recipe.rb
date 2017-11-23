afa8bb424a3a2010a22fd1f0dc1d15acb9e5e573
jeffrey henderson <jeffrey.henderson17@gmail.com>
2017-11-23 18:09:30 +0000

    automatically backed up by learn

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,18 @@
 class Recipe < ActiveRecord::Base
   has_many :ingredients
-  accepts_nested_attributes_for :ingredients 
+  accepts_nested_attributes_for :ingredients
 end
+
+#
+# {
+#   "utf8"=>"✓",
+#        "authenticity_token"=>"ctbg/E9TvCLG9G93s9v7Nm3dUFDKHigSWSEnL6h2Hikea9JbGJEBpmxxA9DINyd9PljQtSru3g8zxZjXHJG3NA==",
+#        "recipe"=> { "title"=>"cake",
+#                     "ingredients_attributes"=> {
+#                                                   "0"=>  {"name"=>"chocolate", "quantity"=>"2 cups"},
+#                                                   "1"=> {"name"=>"vanilla", "quantity"=>"1 cup"}
+#                                                 }
+#             },
+#
+#  "commit"=>"Create Recipe"
+# }
